17b9422ea5eb860d76d124e0a7d54d4776593293
Abhishek-k28 <abhishekkan0jia1111@gmail.com>
2025-11-18 19:47:27 +0530

    manage cors

--- server.js
+++ server.js
@@ -14,9 +14,12 @@ const PORT = process.env.PORT || 4000;
 
 
 //Middleware
+app.use(cors({
+  origin:" http://localhost:5173",
+  credentials: true,
+}))
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
 
 //api endpoints
 
